fb03512cbb8829592587a4d8a723acd37451f8b2
unknown <jamie.g.ii@hotmail.com>
2025-03-14 16:55:48 -0500

    Made a weapon class that a hunter can use and assigned it random values for damage and urability. Need to work on the battle system still but looking like it is kind of coming together.

--- monster.py
+++ monster.py
@@ -7,7 +7,8 @@ class Hunter:
         self.level = 0
         self.exp = 10
         self.hunterHealth = 15
-        
+    
+    #store a weapon object inside of the weapon    
     def earnWeapon(self, weapon):
         self.weapon = weapon
               
@@ -22,6 +23,14 @@ class Hunter:
         if self.exp >= 100:
             self.level += 1
             
+#weapon class for correct attributes
+class Weapon:
+    def __init__(self, weaponName, damage, durability):
+        self.weaponName = weaponName
+        self.damage = damage
+        self.durability = durability
+
+            
 class Monster:
     def __init__(self, name, health, attack, defense):
         self.name = name
@@ -29,9 +38,11 @@ class Monster:
         self.attack = attack
         self.defense = defense
         
-    def takeDamage(self, amount):
-        self.health -= amount
-        print("Monster health is now: " + self.health)
+    def takeDamage(self, weapon):
+        self.health -= weapon.damage
+        weapon.durability -= 1
+        print(f"Monster health is now: {self.health}")
+        print(f"Player health is {Hunter.hunterHealth}")
         
     def isDefeated(self):
         if self.health <= 0:
@@ -47,8 +58,10 @@ class BattleSystem:
         pass
     
     def startBattle(hunter, monster):
-        if hunter.hunterHealth > 0 and monster.health > 0:
-            print(f"You have started a battle with {monster.name}.")
+        print(f"You have started a battle with {monster.name}.")
+        
+        # while hunter.hunterHealth > 0 and monster.health > 0:
+        #     hunter.
 
 #get random monster for user to fight
 def randomMonster():
@@ -60,73 +73,66 @@ def randomMonster():
                "Guardian Arkveld", "Rathian", "Guardian Fulgur Anjanath"]
     return random.choice(monster)
 
+
+#print valid weapons
+def printWeaponNames():
+    print("\nI will now present you with a selection of weapons for you to choose from, they are: ")
+    weaponChoice = ["Great Sword","Long Sword", "Sword and Shield", "Dual Blades", "Hammer",
+                    "Hunting Horn", "Gunlance", "Switch Axe","Charge Blade", "Insect Glaive",
+                    "Light Bow Gun", "Heavy Bow Gun", "Bow"]
+    for w in weaponChoice:
+        print(f" - {w}")
+
+#make sure the user is using a weapon available
+def correctWeapon(weapon):
+    weaponNames = ["Great Sword","Long Sword", "Sword and Shield", "Dual Blades", "Hammer",
+                    "Hunting Horn", "Gunlance", "Switch Axe","Charge Blade", "Insect Glaive",
+                    "Light Bow Gun", "Heavy Bow Gun", "Bow"]
+    
+    return weapon in weaponNames
+    #SIMPLIFIED TO THE ABOVE CODE
+    # if weapon not in weaponNames:
+    #     return False
+    # else:
+    #     return True
+
+
 #deal with the user input
 def getUserInput():
     name = input("What is your name: ")
     age = int(input("What is your age: "))
-    weapon = input("What weapon would you like to use: ")
+    
+    printWeaponNames()
+    
+    #check to see if user input matches a valid weapon
+    #while loop to check user input
+    #TODO: Make sure the user doesn't have to worry about capitilization
+    while True:
+        weapon = input("\nWhat weapon would you like to use: ")
+        if correctWeapon(weapon):
+            break
+        print("Please choose a correct weapon.")
+            
     
     hunter = Hunter(name, age)
-    hunter.earnWeapon(weapon)
+    validWeapon = Weapon(weapon, random.randint(10, 15), random.randint(1,5))
+    hunter.earnWeapon(validWeapon)
+    print(f"Damage = {hunter.weapon.damage}, Durability = {hunter.weapon.durability}")
     
-    #hunter.canHunt()
     if hunter.canHunt() == False:
-        print(f"\nHello {name}, age {age}. I see you have a {weapon}, but I am sorry you must turn back. You are too young to go out and fight. Please come back once you are of age.")
+        print(f"\nHello {name}, age {age}. I see you have chosen a {weapon}, but I am sorry you must turn back. You are too young to go out and fight. Please come back once you are of age.")
+        return -1
     else:
-        print(f"\nHello {name}, age {age}. I see you have a {weapon} with you. You meet the age requirement, welcome on in.")
-        
+        print(f"\nHello {name}, age {age}. I see you have chosen a {weapon}. You meet the age requirement, welcome on in.")
+    
+    #make the monster
     newMonster = randomMonster()
     anotherMonster = Monster(newMonster, 14, 4, 3)
     print("\nThe monster that you have been tasked with fighting is " + newMonster + ".")
     anotherMonster.displayStats()
-    BattleSystem.startBattle(hunter, anotherMonster)
+
 #run silly little program
 getUserInput()
-
-
-
-
-
-
-       
-
         
-        
-#monsterHunterWeapons = ["Great Sword","Long Sword", "Sword and Shield", "Dual Blades", "Hammer",
-                    # "Hunting Horn", "Gunlance", "Switch Axe","Charge Blade", "Insect Glaive",
-                    # "Light Bow Gun", "Heavy Bow Gun", "Bow"]
-
-
-# ranged = [10,11,12]
-# melee = list(range(10))
-
-# monsterList = ["Doshaguma", "Balahara", "Chatacabra", "Rathalos", "Dalthydon", 
-#                "Yian kut-ku", "Lala Barina", "Rey Dau", "Arkveld", "Quematrice",
-#                "Uth Duna", "Gore Magala","Congalala", "Gypceros", "Gravios", "Nerscylla",
-#                "Blangonga", "Nu Udra", "Rompopolo", "Ajarkan", "Hirabami", "Jin Dahaad",
-#                "Guardian Doshaguma", "Guardian Rathalos","Xu Wu", "Guardian Ebony Odogaron",
-#                "Guardian Arkveld", "Rathian", "Guardian Fulgur Anjanath"]
-
-# monster = random.choice(monsterList)
-# weapon = random.choice(monsterHunterWeapons)
-
-# #print all weapons
-# print("\nThe list of weapons are: ")
-# for weapon_name in monsterHunterWeapons:
-#     print(f" - {weapon_name}")
-
-# print("\nThe melee weapons are:")
-# print(", ".join(monsterHunterWeapons[i] for i in melee))
-
-# print("\nThe ranged weapons are: ")
-# print(", ".join(monsterHunterWeapons[i] for i in ranged))
-    
-# #print a random monster to hunt with a random weapon
-# print(f"\nThe monster you are tasked to hunt this time is {monster} with the weapon {weapon}")
-
-# asking = input("\nDo you prefer a ranged or melee weapon: ")
-
-# if asking == "melee":
-#     print("Your weapon will be: " + random.choice(monsterHunterWeapons))
-# else:
-#     print ("Your weapon will be: " + monsterHunterWeapons[i] for i in ranged)
+#def runBattle():   
+    #BattleSystem.startBattle(hunter, anotherMonster)
